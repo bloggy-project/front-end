0e4aa2805d7928f949a075c804b5405115d6a769
HYK424 <turtle424@naver.com>
2023-06-13 12:37:53 +0900

    Chore: 테스트 범위 세분화 프로퍼티 추가

--- jest.config.js
+++ jest.config.js
@@ -21,12 +21,17 @@ const customJestConfig = {
   testMatch: [
     '<rootDir>/**/*.test.(js|jsx|ts|tsx)',
     '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/__tests__/*.(js|jsx|ts|tsx))',
-  ],
+  ], //Jest가 테스트 파일을 검색하는 데 사용할 패턴을 지정
   moduleNameMapper: {
     // Handle module aliases
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
-    "^@/components/(.*)$": "<rootDir>/components/$1",
+    '^@/components/(.*)$': '<rootDir>/components/$1',
   },
+  collectCoverageFrom: [
+    '<rootDir>/src/components/**/*.{ts, tsx}',
+    '!**/node_modules/**',
+    '!**/*.stories',
+  ],//테스트에 포함할 정보의 범위 지정 및 제외
   verbose: true,
   globals: {
     'ts-jest': {
