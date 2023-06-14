4de307836fed251a185198ec8e8d74c7feb7a49a
HYK424 <turtle424@naver.com>
2023-06-15 04:29:29 +0900

    Feat: 기본 로고 컴포넌트 추가

--- src/tests/components/navbar.test.tsx
+++ src/tests/components/navbar.test.tsx
@@ -2,26 +2,8 @@ import { render, screen } from '@testing-library/react';
 import UserEvent from '@testing-library/user-event';
 import NavbarButton from '@/components/structure/Navbar/NavbarButton';
 import Modal from '@/components/common/Modal/Modal';
-import Logo from '@/components/common/Logo/Logo';
-import { useRouter } from 'next/router';
 
-jest.mock('next/router', () => ({
-  useRouter: jest.fn().mockReturnValue({
-    push: jest.fn(),
-  }),
-}));
-//jest.mock의 두 번재 인수는 '객체를 반환하는' 팩토리 함수.
-//'next/router'를 모킹 => jest.fn()을 통해 useRoter를 모킹한 객체 반환
-//mockReturnValue() => useRouter는 push를 모킹하여 반환한다.
-describe('NavBar Test : Logo, Button, Modal', () => {
-  it('Logo click', async () => {
-    render(<Logo />);
-    const bloggyLogo = screen.getByTestId('img');
-
-    await UserEvent.click(bloggyLogo);
-    expect(useRouter().push).toHaveBeenCalledTimes(1);
-  });
-
+describe('NavBar Test : Button, Modal', () => {
   it('NavbarButton render', () => {
     render(<NavbarButton />);
     const button = screen.getByRole('button', {
