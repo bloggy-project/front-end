52095daa95c8a8a10bf63ac63230d7aec6e871f9
HYK424 <turtle424@naver.com>
2023-06-15 04:55:02 +0900

    Feat: 기본 모달 컴포넌트 추가

---
+++ src/components/common/Modal/Modal-Styled.ts
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+export const StyledModal = styled.div`
+  width: 120px;
+  height: 20px;
+  background-color: red;
+  z-index: 99;
+  opacity: 1;
+`;
---
+++ src/components/common/Modal/Modal.tsx
@@ -0,0 +1,29 @@
+import ModalWrapper from './ModarWrapper';
+import { StyledModal } from './Modal-Styled';
+
+type ModalProps = {
+  isOpen: boolean;
+  toggleModal: () => void;
+};
+
+const Modal = ({ isOpen, toggleModal }: ModalProps) => {
+  const onlyToggledInWrapper = (e: React.MouseEvent) => {
+    if (e.target !== e.currentTarget) {
+      return;
+    } else {
+      toggleModal();
+    }
+  };
+
+  return (
+    <>
+      {isOpen && (
+        <ModalWrapper onClick={onlyToggledInWrapper}>
+          <StyledModal data-testid="modal">모달</StyledModal>
+        </ModalWrapper>
+      )}
+    </>
+  );
+};
+
+export default Modal;
