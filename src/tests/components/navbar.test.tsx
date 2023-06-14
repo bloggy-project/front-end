110a0e533228cabdab8b286b68a77f73a45f55e8
HYK424 <turtle424@naver.com>
2023-06-15 04:04:41 +0900

    Test: Navbar 구성 컴포넌트 통합 테스트

--- src/tests/components/navbar.test.tsx
+++ src/tests/components/navbar.test.tsx
@@ -1,26 +1,48 @@
 import { render, screen } from '@testing-library/react';
 import UserEvent from '@testing-library/user-event';
-import AuthButton from '@/components/structure/Navbar/NavbarButton';
+import NavbarButton from '@/components/structure/Navbar/NavbarButton';
+import Modal from '@/components/common/Modal/Modal';
+import Logo from '@/components/common/Logo/Logo';
+import { useRouter } from 'next/router';
 
-jest.mock('@/components/common/Modal/Modal', () => {
-  const mockModal = () => {
-    return <div id="dummy-modal"></div>;
-  };
-  return mockModal;
-});
+jest.mock('next/router', () => ({
+  useRouter: jest.fn().mockReturnValue({
+    push: jest.fn(),
+  }),
+}));
+//jest.mock의 두 번재 인수는 '객체를 반환하는' 팩토리 함수.
+//'next/router'를 모킹 => jest.fn()을 통해 useRoter를 모킹한 객체 반환
+//mockReturnValue() => useRouter는 push를 모킹하여 반환한다.
+describe('NavBar Test : Logo, Button, Modal', () => {
+  it('Logo click', async () => {
+    render(<Logo />);
+    const bloggyLogo = screen.getByTestId('img');
 
-const { container } = render(<AuthButton />);
-const user = UserEvent.setup();
-
-describe('로그인/ 회원가입 버튼', () => {
-  const AuthButton = screen.getByRole('button', {
-    name: '로그인 / 회원가입',
+    await UserEvent.click(bloggyLogo);
+    expect(useRouter().push).toHaveBeenCalledTimes(1);
   });
-  it('render', () => {
-    expect(AuthButton).toBeInTheDocument();
+
+  it('NavbarButton render', () => {
+    render(<NavbarButton />);
+    const button = screen.getByRole('button', {
+      name: '로그인 / 회원가입',
+    });
+    expect(button).toBeInTheDocument();
   });
-  it('child render on click', async () => {
-    user.click(AuthButton);
-    expect(container.querySelector('#dummy-modal')).toBeInTheDocument();
+
+  it('Modal render', () => {
+    render(<Modal isOpen={true} toggleModal={jest.fn()} />);
+    const modal = screen.getByTestId('modal');
+    expect(modal).toBeInTheDocument();
+  });
+
+  it('Modal render on click', async () => {
+    render(<NavbarButton />);
+    const button = screen.getByRole('button', {
+      name: '로그인 / 회원가입',
+    });
+    await UserEvent.click(button);
+    const modal = screen.getByTestId('modal');
+    expect(modal).toBeInTheDocument();
   });
 });
