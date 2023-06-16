f61c8c9e32574633d9021c6486ca87bf2eb97f31
HYK424 <turtle424@naver.com>
2023-06-16 13:38:44 +0900

    Feat: 탭 컴포넌트 추가(인덱스 페이지)

---
+++ src/tests/tab.test.tsx
@@ -0,0 +1,15 @@
+import { render, screen } from '@testing-library/react';
+import UserEvent from '@testing-library/user-event';
+import Tab from '../app/_components/Tab/Tab';
+
+describe('Tab Test', () => {
+  it('Tab change on click', async () => {
+    render(<Tab />);
+    const button = screen.getByRole('button', {
+      name: '게시물',
+    });
+    await UserEvent.click(button);
+    const contents = screen.getByTestId('blogger-contents');
+    expect(contents).toBeInTheDocument();
+  });
+});
