6f4438110f43279b736c7c56063ad88b5dbd3159
HYK424 <turtle424@naver.com>
2023-06-19 22:37:39 +0900

    Feat: 외부 클릭 이벤트 감지하는 토글 컴포넌트 추가

---
+++ src/hooks/useToggle.ts
@@ -0,0 +1,31 @@
+import { useState, RefObject, useEffect } from 'react';
+
+const useToggle = (ref?: RefObject<HTMLElement>) => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const onChangeOpen = () => {
+    setIsOpen((isOpen) => !isOpen);
+  };
+
+  const closeByOutside = (e: MouseEvent) => {
+    if (ref?.current && !ref.current?.contains(e.target as Node)) {
+      setIsOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (ref) {
+      document.addEventListener('mousedown', closeByOutside);
+      return () => {
+        document.removeEventListener('mousedown', closeByOutside);
+      };
+    }
+  }, [ref?.current]);
+
+  return {
+    isOpen,
+    onChangeOpen,
+  };
+};
+
+export default useToggle;
