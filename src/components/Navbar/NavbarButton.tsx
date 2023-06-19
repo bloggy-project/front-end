8628a166b16a7459e89f1706772d8300d2f6f032
HYK424 <turtle424@naver.com>
2023-06-20 06:52:49 +0900

    Style: 컴포넌트 프로퍼티명 변경

--- src/components/Navbar/NavbarButton.tsx
+++ src/components/Navbar/NavbarButton.tsx
@@ -1,20 +1,20 @@
 import Button from '@/components/Button/Button';
 import { PALETTE } from '@/assets/color';
 import Modal from '@/components/Modal/Modal';
-import useModal from '@/hooks/useModal';
+import useToggle from '@/hooks/useToggle';
 
 const NavbarButton = () => {
-  const { isOpen, toggleModal } = useModal();
+  const { isOpen, onChangeOpen } = useToggle();
   return (
     <>
       <Button
-        onClick={toggleModal}
+        onClick={onChangeOpen}
         hover={'opacity'}
         size={'sm'}
         color={PALETTE.SPOT1}
         content={'로그인 / 회원가입'}
       />
-      <Modal isOpen={isOpen} toggleModal={toggleModal} />
+      <Modal isOpen={isOpen} onChangeOpen={onChangeOpen} />
     </>
   );
 };
