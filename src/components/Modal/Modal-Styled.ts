bcab00c797b131283d482194852dbf6c97426fcd
HYK424 <turtle424@naver.com>
2023-06-23 00:30:48 +0900

    Style: 모달 등장 시 뒷배경 스크롤 금지

--- src/components/Modal/Modal-Styled.ts
+++ src/components/Modal/Modal-Styled.ts
@@ -2,9 +2,11 @@ import styled from 'styled-components';
 
 export const StyledModal = styled.div`
   position: absolute;
-  top: calc(50vh - 100px); left: calc(50vw - 200px);
+  top: calc(50vh - 100px);
+  left: calc(50vw - 200px);
   background-color: white;
-  display: flex; justify-content: center;
+  display: flex;
+  justify-content: center;
   align-items: center;
   border-radius: 10px;
   width: 400px;
--- src/components/Modal/ModalWrapper-Styled.ts
+++ src/components/Modal/ModalWrapper-Styled.ts
@@ -2,7 +2,10 @@ import styled from 'styled-components';
 
 export const StyledModalWrapper = styled.div`
   position: fixed;
-  top:0; left: 0; bottom: 0; right: 0;
+  top: 0;
+  left: 0;
+  bottom: 0;
+  right: 0;
   /* display: flex;
   align-items: center;
   justify-content: center; */
