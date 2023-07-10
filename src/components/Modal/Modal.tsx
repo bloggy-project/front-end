04a3043ca7ae0f8fc93f092f8a53a38a385b3113
HYK424 <turtle424@naver.com>
2023-07-10 16:50:10 +0900

    Refactor: 스크롤 오버플로우 로직 분리 -> 커스텀 훅

--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,26 +1,11 @@
 'use client';
 import ModalWrapper from './ModalWrapper';
 import { StyledModal } from './Modal-Styled';
-import { useEffect } from 'react';
 import AuthTheme from '../ModalContent/ModalContent';
 import modalStore from '@/store/modalStore';
 
 const Modal = () => {
   const isOpenModal = modalStore((state) => state.isOpenModal);
-
-  useEffect(() => {
-    if (isOpenModal) {
-      const openModal = () => {
-        document.body.style.overflow = 'hidden'; // Disable body scrolling
-      };
-      openModal();
-    } else {
-      const closeModal = () => {
-        document.body.style.overflow = 'auto'; // Enable body scrolling
-      };
-      closeModal();
-    }
-  }, [isOpenModal]);
 
   return (
     <>
--- src/components/Navbar/Naverbar.tsx
+++ src/components/Navbar/Naverbar.tsx
@@ -2,9 +2,13 @@ import Logo from '@/components/Logo/Logo';
 import { NavbarWrapper } from './NavbarWrapper-Styled';
 import NavbarButton from './NavbarButton';
 import useRefreshToken from '../../hooks/useRefreshToken';
+import modalStore from '@/store/modalStore';
+import useScrollOverflow from '@/hooks/useScrollOverflow';
 
 const Navbar = () => {
   const accessToken = useRefreshToken();
+  const isOpenModal = modalStore((state) => state.isOpenModal);
+  useScrollOverflow(isOpenModal);
   return (
     <NavbarWrapper>
       <Logo />
