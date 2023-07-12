15350c46ecf3995680d3ae0e82800faed9747ff0
HYK424 <turtle424@naver.com>
2023-07-12 17:20:01 +0900

    Refactor: metadata 위치 변경

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,11 @@
 import Main from './_components/Main/Main';
 import SelectContainer from './_components/SelectContainer/SelectContainer';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'bloggy.kro.kr',
+  description: 'welcome to bloggy',
+};
 
 export default function Home() {
   return (
