262fde33a22ffaf112dc17c1f6a36128dbb5f9ef
HYK424 <turtle424@naver.com>
2023-07-12 23:28:24 +0900

    Refactor: next.config.js에서 .env 불러오기 삭제 -> 정적 정의

--- src/lib/api/config/index.ts
+++ src/lib/api/config/index.ts
@@ -1,14 +1,16 @@
 import axios from 'axios';
 
+export const public_api_url = 'https://bloggy.kro.kr/api';
+
 export const api = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
+  baseURL: public_api_url,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export const apiPrivate = axios.create({
-  baseURL: `${process.env.NEXT_PUBLIC_API_URL}`,
+  baseURL: public_api_url,
   headers: {
     'Content-Type': 'application/json',
   },
