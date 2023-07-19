b0fcc7a0df518ba3014be85527a4e31dc957d928
HYK424 <turtle424@naver.com>
2023-07-19 17:36:37 +0900

    Feat: 유저 썸네일 수정 기능 추가

--- src/hooks/useThumbnail.ts
+++ src/hooks/useThumbnail.ts
@@ -1,18 +1,28 @@
 import { useState } from 'react';
 
 const useThumbnail = (img: string | null) => {
+  const default_thumbnail = '/default_thumbnail.png';
   const checkThumbnail = (img: string | null): string => {
     if (!img) {
-      const default_thumbnail = '/default_thumbnail.png';
       return default_thumbnail;
     }
     return img;
   };
 
-  const [thumbnailImg] = useState(checkThumbnail(img));
+  const [thumbnailImg, setThumbnailImg] = useState(checkThumbnail(img));
+
+  const setDefaultImg = () => {
+    setThumbnailImg(default_thumbnail);
+  };
+
+  const setThumbImg = (img: string) => {
+    setThumbnailImg(img);
+  };
 
   return {
     thumbnailImg,
+    setThumbImg,
+    setDefaultImg,
   };
 };
 
