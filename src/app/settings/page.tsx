e64448f1712bff94cb1a585e19546fc3fc0fffe7
HYK424 <turtle424@naver.com>
2023-07-27 18:52:19 +0900

    Feat: /settings 접근 권한 페이지 추가

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,8 +1,16 @@
 'use client';
+import authStore from '@/store/authStore';
 import Settings from './components/Settings/Settings';
+import { StatusToken } from '@/assets/status';
+import { notFound } from 'next/navigation';
 
 const PageSettings = () => {
-  return <Settings />;
+  const accessToken = authStore((state) => state.accessToken);
+  if (accessToken === StatusToken.Initial || StatusToken.No_token) {
+    notFound();
+  } else {
+    return <Settings />;
+  }
 };
 
 export default PageSettings;
