f65d4c5b81a376368e011742fd8744f31bde8428
HYK424 <turtle424@naver.com>
2023-07-31 20:13:17 +0900

    Refactor: store에서 유저 정보 state 삭제

--- src/store/authStore.ts
+++ src/store/authStore.ts
@@ -1,41 +1,19 @@
 import { StatusToken } from '@/assets/status';
-import MyLocalStorage from '@/lib/handler/handleLocalStorage';
 import { create } from 'zustand';
 import { devtools } from 'zustand/middleware';
 
-export type UserInfo = {
-  thumbnail: null | string;
-  name: string;
-  email: string;
-};
-
 type authState = {
   accessToken: string | null;
-  userInfo: UserInfo;
   setAccessToken: (value: string | null) => void;
-  setUserInfo: (value: UserInfo) => void;
 };
-
-const storageName = 'userinfo';
 
 const authStore = create<authState>()(
   devtools((set) => ({
     accessToken: StatusToken.Initial,
-    userInfo: {
-      thumbnail: '',
-      name: '',
-      email: '',
-    },
     setAccessToken: (value) =>
       set({
         accessToken: value,
       }),
-    setUserInfo: (value) => (
-      set({
-        userInfo: value,
-      }),
-      new MyLocalStorage<UserInfo>(storageName).set(value)
-    ),
   })),
 );
 
