9454e0bfe3f2e6bcb820f7ec108c812dc2ccec5c
HYK424 <turtle424@naver.com>
2023-08-01 16:53:00 +0900

    Refactor: env 파일 추가에 따른 설정 변경

--- src/lib/api/config/index.ts
+++ src/lib/api/config/index.ts
@@ -1,16 +1,16 @@
 import axios from 'axios';
 
-export const public_api_url = 'https://bloggy.kro.kr/api';
+const BASE_API_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 
 export const api = axios.create({
-  baseURL: public_api_url,
+  baseURL: BASE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
 export const apiPrivate = axios.create({
-  baseURL: public_api_url,
+  baseURL: BASE_API_URL,
   headers: {
     'Content-Type': 'application/json',
   },
