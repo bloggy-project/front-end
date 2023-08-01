51dda929860804ed660bbfa979359f5e198568d3
HYK424 <turtle424@naver.com>
2023-08-01 16:53:00 +0900

    Refactor: env 파일 추가에 따른 설정 변경

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
+
+const SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL;
+const CDN_DOMAIN = process.env.NEXT_PUBLIC_CDN_DOMAIN;
+
 const nextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -11,7 +15,7 @@ const nextConfig = {
   swcMinify: true, //swc 빌드 툴 사용 옵션
   compiler: {
     styledComponents: true,
-    //최종 빌드 주석 해제
+    //최종 빌드 시 해당 주석 해제
     // removeConsole: {
     //   //콘솔 호출 제거
     //   exclude: ['error'], //error 관련 콘솔은 출력
@@ -21,18 +25,14 @@ const nextConfig = {
     return [
       {
         source: '/:path*', //api request path
-        destination: `https://bloggy.kro.kr/api/:path*`, //목적 path
+        destination: `${SERVER_URL}/:path*`, //목적 path
       },
     ];
   },
   images: {
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    domains: [
-      'thumb.mt.co.kr',
-      'img4.daumcdn.net',
-      'dzbnjlxurlvs4.cloudfront.net',
-    ],
+    domains: ['thumb.mt.co.kr', 'img4.daumcdn.net', `${CDN_DOMAIN}`],
   },
 };
 
