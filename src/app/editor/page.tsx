07575a0a3c3e176dcf2d997eb506f47a62b64a20
HYK424 <turtle424@naver.com>
2023-08-07 20:29:23 +0900

    Feat: /editor 페이지 접근 권한 처리

--- src/app/editor/page.tsx
+++ src/app/editor/page.tsx
@@ -2,23 +2,21 @@
 import authStore from '@/store/authStore';
 import { StatusToken } from '@/assets/status';
 import { notFound } from 'next/navigation';
-import EditNoSSR from './components/EditMode/EditNoSSR';
 import EditPost from './components/EditPost/EditPost';
 import ModalEditor from './components/ModalEditor/ModalEditor';
 
 const PageSettings = () => {
-  //   const accessToken = authStore((state) => state.accessToken);
-  //   if (accessToken === (StatusToken.Initial || StatusToken.No_token)) {
-  //     notFound();
-  //   } else {
-  //     return <EditNoSSR />;
-  //   }
-  return (
-    <>
-      <EditPost />
-      <ModalEditor />
-    </>
-  );
+  const accessToken = authStore((state) => state.accessToken);
+  if (accessToken === (StatusToken.Initial || StatusToken.No_token)) {
+    notFound();
+  } else {
+    return (
+      <div>
+        <EditPost />
+        <ModalEditor />
+      </div>
+    );
+  }
 };
 
 export default PageSettings;
