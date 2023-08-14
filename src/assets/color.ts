c48232ec9705b40b879dcd824c06198479fe994b
HYK424 <turtle424@naver.com>
2023-08-14 17:41:32 +0900

    Style: 토스트 팝업창 색상 변경

--- src/assets/color.ts
+++ src/assets/color.ts
@@ -5,14 +5,15 @@ export const Palette = {
   SPOT2: '#D8F3FF', //매우 연한 하늘색
   TWISTED1: '#E19F20', //주황색
   TWISTED2: '#FFEDCB', //매우 연한 주황색
+  TWISTED3: '#B67B00', //갈색
   SWITCH: '#E19F20', //진한 주황색
   NATURAL: '#7094BC', //잿빛
   SPOT2_TEXT: '#344A53',
 };
 
 export const Effect = {
-  BACK: '#f4f7fb',
-  SHADOW: '#d5d8df',
-  HOVER_BACK: '#b7b7c3',
-  SELECTED: ' #009df7',
+  BACK: '#f4f7fb', //흰색에 가까운 뒷배경 색깔
+  SHADOW: '#d5d8df', //BACK보다 살짝 진한 회색
+  HOVER_BACK: '#b7b7c3', //BACK보다 진한 회색
+  SELECTED: ' #009df7', //하늘색보다 짙은 밝은 채도 파랑
 };
