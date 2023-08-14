48167857a39137f7313814dfce50998fe255fdcf
HYK424 <turtle424@naver.com>
2023-08-14 17:41:32 +0900

    Style: 토스트 팝업창 색상 변경

--- src/components/Toast/ToastCustom_Styled.ts
+++ src/components/Toast/ToastCustom_Styled.ts
@@ -1,3 +1,4 @@
+import { Palette } from '@/assets/color';
 import { ToastContainer } from 'react-toastify';
 import styled from 'styled-components';
 
@@ -6,5 +7,15 @@ export const StyledToast = styled(ToastContainer)`
     font-size: 1.4rem;
     border-radius: 1rem;
     padding: 1.2rem;
+  }
+
+  .Toastify__toast-icon {
+    & > svg {
+      fill: ${Palette.SPOT1};
+    }
+  }
+
+  .Toastify__progress-bar--success {
+    background-color: ${Palette.SPOT1};
   }
 `;
